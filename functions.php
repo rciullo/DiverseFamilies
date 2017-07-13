82fea0d26a0a38d4e5cdcd1fe8d69c1f7a439dc4
Bobby Ciullo <bobby.ciullo@ucf.edu>
2017-07-13 10:22:42 -0400

    added new menus

--- functions.php
+++ functions.php
@@ -77,9 +77,8 @@ function register_my_menus() {
   register_nav_menus(
     array(
       'header-menu' => __( 'Header Menu' ),
-      'header-menu-2' => __( 'Header Menu 2'),
+      'small-header-menu' => __( 'Small Header Menu'),
       'side-menu' => __( 'Side Menu' ),
-      'footer-menu' => __( 'Footer Menu Find')
     )
   );
 }
