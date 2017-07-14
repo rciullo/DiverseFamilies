808bbc9fdacaafc1df8c5cb707a6204b5d757c9c
Bobby Ciullo <bobby.ciullo@ucf.edu>
2017-07-14 15:43:10 -0400

    added book-feed shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -2,4 +2,22 @@
 
 // Website shortcodes
 
+function books_feed( $atts, $content = null ) {
+  extract(shortcode_atts( array(
+      'id' => 'books_feed',
+      'url' => 'http://stars.library.ucf.edu/diversefamilies-familyrelationships/bookfeed.rss',
+      'number' => '5'
+  ), $atts ));
+  $output = '
+  <div id="'.$id.'"></div>
+  <script>
+    $(document).ready( function() {
+      diverse_families_feed("'.$id.'", "'.$url.'", '.$number.');
+    });
+  </script>
+  ';
+  return $output;
+}
+add_shortcode('books-feed', 'books_feed');
+
 ?>
