d295589221c58e4be5a604caf58a51edc163e2cf
Bobby Ciullo <bobby.ciullo@ucf.edu>
2017-07-26 14:23:40 -0400

    removed typo from search placeholder text

--- searchform.php
+++ searchform.php
@@ -8,7 +8,7 @@ Description: Code for the website search form.
   <fieldset>      
     <label for="search" class="sr-only">Search </label>          
     <div class="input-group">
-      <input class="form-control" type="text" name="q" class="search" id="search" placeholder="Search Collection s">
+      <input class="form-control" type="text" name="q" class="search" id="search" placeholder="Search Collection">
       <!-- <input class="form-control" id="search" name="q" value="" type="text" placeholder="Search Collection">  -->
       <span class="input-group-btn">  
         <button type="submit" class="btn btn-default" name="query" value="Search"><span class="glyphicon glyphicon-search"></span><span class="sr-only">Search</span></button>
