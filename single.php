2089d830460d2e227af0ce628797b96ddcfaeba0
Bobby Ciullo <bobby.ciullo@ucf.edu>
2018-11-07 13:41:52 -0500

    removed social buttons and shifted gray background

--- single.php
+++ single.php
@@ -3,22 +3,20 @@
 	<div id="title_bar" class="container">
 	<!-- single.php -->
 		<div class="row">
-			<div class="col-sm-8">
+			<div class="col-sm-12">
 				<header><h1><a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ) ?>">News &amp; Blog</a></h1></header>
 				<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 			</div>
-			<div class="col-sm-4">
-				<div class="header-search"><?php get_search_form(); ?></div>
-			</div>
 		</div>
 	</div>
-	<div  class="background-color-gray">
+
 		<div id="content" class="container">
 			<div class="row">
 				<div id="sidebar" class="col-sm-3">
 					<?php get_sidebar(); ?>
 				</div>
 				<div id="content_area" class="col-sm-9">
+					<div  class="background-color-gray">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<?php	
 						$categories = get_the_category();
@@ -38,32 +36,6 @@
 							<div class="post-header-img"><img src="<?php echo(get_template_directory_uri()) ?>/images/generic-default-banner.jpg"></div>
 						<?php endif; ?>
 							<div class="news-post-content">
-								<div class="share-btn-group">
-	 								<a class="share-btn facebook-btn" href="javascript:share_button('http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>', 520, 350)">
-										<span class="fa-stack fa-lg">
-											<i class="fa fa-circle fa-stack-2x"></i>
-											<i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-										</span>
-									</a>
-									<a class="share-btn twitter-btn" href="javascript:share_button('http://twitter.com/share?url=<?php echo get_permalink(); ?>&text=<?php echo get_the_title(); ?>', 500, 500)">
-										<span class="fa-stack fa-lg">
-											<i class="fa fa-circle fa-stack-2x"></i>
-											<i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-										</span>
-									</a>
-									<a class="share-btn gplus-btn" href="javascript:share_button('https://plus.google.com/share?url=<?php echo get_permalink(); ?>', 500, 500)">
-										<span class="fa-stack fa-lg">
-											<i class="fa fa-circle fa-stack-2x"></i>
-											<i class="fa fa-google-plus fa-stack-1x fa-inverse"></i>
-										</span>
-									</a>
-									<a class="share-btn email-btn" href="mailto:?subject=<?php echo get_the_title(); ?>&body=<?php echo get_the_title(); ?> <?php echo get_permalink(); ?>">
-										<span class="fa-stack fa-lg">
-											<i class="fa fa-circle fa-stack-2x"></i>
-											<i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
-										</span>
-									</a>		
-								</div>
 								<div class="news-post-title">
 									<header>
 					                  <h2><?php echo get_the_title(); ?></h2>
@@ -75,13 +47,15 @@
 								<div class="clearfix"></div>
 								<?php comments_template( $file, $separate_comments ); ?>
 							</div>
+							</div>
 						</div>
 					</article>
 					<?php endwhile; else: ?>
 					<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 				</div>
+				</div>
 			</div>
 		</div>
-	</div>
+	
 </div>
 <?php get_footer(); ?>
